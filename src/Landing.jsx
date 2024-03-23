f2b3b6ff0350ce3f500852a04be795e30ce7dd68
Dora <cw749@cornell.edu>
2024-03-23 18:12:21 -0400

    footer

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -156,6 +156,7 @@ const Landing = () => {
             </div>
           </div>
         </section>
+        <Footer />
       </div>
     </>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,17 +3,19 @@ import linkedin from "../img/linkedin.svg";
 
 const Footer = () => {
   return (
-    <footer className=" text-[#f6f5f0] w-full bg-slate-500">
-      <div className="flex flex-row justify-between items-center">
+    <footer className=" text-[#f6f5f0] w-full ">
+      <div className="flex flex-row justify-end items-center">
 
         <div className="flex flex-row items-center space-x-8 ">
           <a
             href="https://github.com/dorawengg"
+            target="_blank"
           >
             <img src={git} alt="GitHub Logo" className="w-full h-full" />
           </a>
           <a
             href="https://www.linkedin.com/in/chuyinweng/"
+            target="_blank"
           >
             <img src={linkedin} alt="LinkedIn Logo" className="w-full h-full" />
           </a>
